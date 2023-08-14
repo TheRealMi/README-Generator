ba148059ad15bcffa8c841017724cb443441821c
The Real Mi <miaolmedillo@gmail.com>
2023-08-14 18:32:54 -0400

    wrote preliminary markdown text

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -4,7 +4,9 @@ function renderLicenseBadge(license) {}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  return 
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
@@ -14,6 +16,48 @@ function renderLicenseSection(license) {}
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  ${renderLicenseBadge(data.license)}
+
+  ## Description
+  
+  ${data.description}
+  
+  ## Table of Contents
+    
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [How to Contribute](#howtocontribute)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  
+  ## Installation
+  
+  ${data.installation}  
+
+  ## Usage
+  
+  ${data.usage}
+  
+  ## License
+  
+  ${renderLicenseSection(data.license)}  
+  
+  ## How to Contribute
+  
+  ${data.contribution}  
+
+  ## Tests
+  
+  ${data.test}
+
+  ## Questions
+  <ul>
+    <li> <a href="https://github.com/${data.gitUser}">My Github Profile </a></li>
+    <li> <a href="mailto:${email}"> My Email </a></li>
+  </ul>
+  If you have any questions feel free to reach out via email! 
+  Include "${data.title}" in the subject line
 `;
 }
 
